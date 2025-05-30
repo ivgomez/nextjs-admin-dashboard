8c3ac29361ed6e08bd63a8aae4b293268d33ba61
ivgomez <iv.gomez@hotmail.com>
2025-05-29 20:44:30 -0500

    fixing style issues

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -42,7 +42,7 @@ export default function LoginPage() {
   }
 
   return (
-    <div className='flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8'>
+    <div className='flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-100'>
       <div className='sm:mx-auto sm:w-full sm:max-w-md'>
         <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>Sign in to your account</h2>
         <p className='mt-2 text-center text-sm text-gray-600'>
@@ -72,7 +72,7 @@ export default function LoginPage() {
                   type='email'
                   autoComplete='email'
                   required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  className='block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
@@ -88,7 +88,7 @@ export default function LoginPage() {
                   type='password'
                   autoComplete='current-password'
                   required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  className='block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function RegisterPage() {
   return (
-    <div className='flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8'>
+    <div className='flex min-h-screen flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-200'>
       <div className='sm:mx-auto sm:w-full sm:max-w-md'>
         <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>Create a new account</h2>
         <p className='mt-2 text-center text-sm text-gray-600'>
@@ -27,7 +27,7 @@ export default function RegisterPage() {
                   type='text'
                   autoComplete='name'
                   required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  className='block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
@@ -43,7 +43,7 @@ export default function RegisterPage() {
                   type='email'
                   autoComplete='email'
                   required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  className='block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
@@ -59,7 +59,7 @@ export default function RegisterPage() {
                   type='password'
                   autoComplete='new-password'
                   required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  className='block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
@@ -75,7 +75,7 @@ export default function RegisterPage() {
                   type='password'
                   autoComplete='new-password'
                   required
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                  className='block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                 />
               </div>
             </div>
